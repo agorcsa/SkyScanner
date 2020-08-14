8838ee0c3be359d4433505b9f8cdcd00912e898d
andreeagorcsa <andreea.gorcsa@t-systems.com>
2020-08-14 19:06:45 +0200

    Update the Call instead of List with Object

--- app/src/main/java/com/example/skyscanner/JsonSkyScannerAPI.java
+++ app/src/main/java/com/example/skyscanner/JsonSkyScannerAPI.java
@@ -1,5 +1,7 @@
 package com.example.skyscanner;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 import retrofit2.Call;
@@ -7,12 +9,11 @@ import retrofit2.http.GET;
 import retrofit2.http.Headers;
 
 public interface JsonSkyScannerAPI {
-
     // the result of the Retrofit call will be a list of Place JSON objects
     @Headers({
-            "x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-            "x-rapidapi-key", "548aa9ba87mshcca697ffe0ef206p15a507jsn964a2b1dc9c2"
+            "x-rapidapi-host: skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+            "x-rapidapi-key: 548aa9ba87mshcca697ffe0ef206p15a507jsn964a2b1dc9c2"
     })
-    @GET("places")
-    Call<List<Place>> getPlaces();
+    @GET("UK/GBP/en-GB/?query=Stockholm")
+    Call<Place> getPlaces();
 }
