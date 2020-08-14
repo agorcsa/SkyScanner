8e06e6016022b486a17a8afa1c3b265cd2d9f22c
andreeagorcsa <andreea.gorcsa@t-systems.com>
2020-08-14 19:00:08 +0200

    Update private fields adding @SerializedName

--- app/src/main/java/com/example/skyscanner/Place.java
+++ app/src/main/java/com/example/skyscanner/Place.java
@@ -1,12 +1,20 @@
 package com.example.skyscanner;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Place {
 
+    @SerializedName("PlaceId")
     private String placeId;
+    @SerializedName("PlaceName")
     private String placeName;
+    @SerializedName("CountryId")
     private String countryId;
+    @SerializedName("RegionId")
     private String regionalId;
+    @SerializedName("CityId")
     private String cityId;
+    @SerializedName("CountryName")
     private String countryName;
 
     public String getPlaceId() {
